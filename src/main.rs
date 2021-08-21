f089641e3ff764f1ecb902b7e0cfc37670ece14d
Yuichiro Terada <yuichiro_terada@caddi.jp>
2021-08-21 22:18:16 +0900

    draw INSERT element

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,9 @@ fn main() {
         .get_matches();
     let path = args.value_of("dxf").unwrap();
     let drawing = dxf::Drawing::load_file(path).unwrap();
-    println!("{:?}", drawing.header.minimum_drawing_extents);
-    println!("{:?}", drawing.header.maximum_drawing_extents);
-    println!("{:?}", drawing.header.minimum_drawing_limits);
-    println!("{:?}", drawing.header.maximum_drawing_limits);
     let view_box = {
-        let min = drawing.header.minimum_drawing_limits;
-        let max = drawing.header.maximum_drawing_limits;
+        let min = &drawing.header.minimum_drawing_limits;
+        let max = &drawing.header.maximum_drawing_limits;
         (min.x, min.y, max.x - min.x, max.y - min.y)
     };
     let mut svg = svg::Document::new().set("viewBox", view_box).add(
@@ -22,11 +18,32 @@ fn main() {
             .set("width", view_box.2 - view_box.0)
             .set("height", view_box.3 - view_box.1),
     );
-    for e in &drawing.entities {
+    svg = draw_entities(svg, &drawing.entities, &drawing);
+    svg::save(args.value_of("svg").unwrap(), &svg).unwrap();
+}
+
+fn draw_entities(
+    mut svg: svg::Document,
+    entities: &[dxf::entities::Entity],
+    drawing: &dxf::Drawing,
+) -> svg::Document {
+    for e in entities {
         match &e.specific {
             dxf::entities::EntityType::Line(line) => {
-                println!("LINE");
                 svg = draw_line(svg, line);
+            }
+            dxf::entities::EntityType::Insert(insert) => {
+                println!("INSERT: {}", insert.name);
+                println!("{:?}", insert);
+                if let Some(block) = drawing
+                    .blocks
+                    .iter()
+                    .find(|block| block.name == insert.name)
+                {
+                    svg = draw_entities(svg, &block.entities, drawing);
+                } else {
+                    println!("block not found: name = {}", insert.name);
+                }
             }
             dxf::entities::EntityType::Circle(circle) => {
                 println!("CIRCLE");
@@ -34,10 +51,10 @@ fn main() {
             dxf::entities::EntityType::Polyline(pol) => {
                 println!("POLYLINE");
             }
-            _ => println!("other"),
+            _ => (),
         }
     }
-    svg::save(args.value_of("svg").unwrap(), &svg).unwrap();
+    svg
 }
 
 fn draw_line(svg: svg::Document, line: &dxf::entities::Line) -> svg::Document {
